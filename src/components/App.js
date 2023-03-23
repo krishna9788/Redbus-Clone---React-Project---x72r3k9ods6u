e8e972697099aef3a5a8c147dfd8cfc34a0db89f
krishna9788 <krishnakushwaha978@gmail.com>
2023-03-23 18:31:29 +0530

    Project

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,32 @@
 import React from 'react'
 import '../styles/App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './home/Home';
+import BusList from './booking/BusList';
+import Nav from './layouts/Nav';
+import NotFound from './general/NotFound';
+import PrintTicket from './booking/PrintTicket';
+import Footer from './home/Footer';
+
+
 const App = () => {
 
 
   return (
-    <div id="main">
-    </div>
+
+    <BrowserRouter>
+        <Nav />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/bus-tickets' element={<BusList />} /> 
+        <Route path='/booked-ticket' element={<PrintTicket />} /> 
+        <Route path='/*' element={<NotFound />} /> 
+
+
+
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   )
 }
 
